95078c256b2ee402e8346bb829e7a8b9bbe8f334
QY <watermelonqyu@gmail.com>
2020-06-14 20:07:37 -0500

    initial commit

---
+++ src/main/java/com/test/example/Application.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.test.example;
+
+/**
+ * @author qyu1
+ *
+ */
+public class Application {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/main/java/com/test/example/calculate/StringManipulate.java
@@ -0,0 +1,18 @@
+package com.test.example.calculate;
+
+/**
+ * @author qyu1
+ *
+ */
+public class StringManipulate {
+	/**
+	 * inputString
+	 */
+	private String inputString;
+	
+	public void method1() {
+		
+	}
+	
+	
+}
